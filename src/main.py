5169187a2770379d4ddbc88216f9b09392d2d211
H6WU6R <haoran.wu@u.nus.edu>
2025-04-02 19:10:43 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,38 +4,38 @@ from A3_Behavioral_Patterns_Analysis.A3_main import main as A3_main
 from A4_Campaign_Impact_Analysis.A4_main import main as A4_main
 from A5_Segmentation_Updates.app import main as A5_app
 from B1_Predicting_Customer_Preferences.B1_main import main as B1_main
-from B3_Measuring_Campaign_ROI.B3_main import main as B3_main
-from B4_Cost_Effectiveness_of_Campaigns.B4_main import main as B4_main
+from B3_Campaign_ROI_Evaluation.B3_main import main as B3_main
+from B4_Cost_Effectiveness_Of_Campaigns.B4_main import main as B4_main
 from B5_Customer_Retention_Strategies.B5_main import main as B5_main
 
 
 if __name__ == "__main__":
 
     print("-------------------------- Running A1: Customer Segmentation --------------------------\n")
-    A1_main()
+    #A1_main()
     
     print("-------------------------- Running A2: Customer Engagement --------------------------\n")
     A2_main()
     
     print("-------------------------- Running A3: Behavioral Patterns --------------------------\n")
-    A3_main()
+    #A3_main()
     
     print("-------------------------- Running A4: Campaign Impact Analysis --------------------------\n")
-    A4_main()
+    #A4_main()
     
     print("-------------------------- Running A5: Segmentation Updates --------------------------\n")
-    A5_app()
+    #A5_app()
 
     print("-------------------------- Running B1: Predicting Customer Preferences --------------------------\n")
-    B1_main()
+    #B1_main()
     
     print("-------------------------- Refer to markdown for Task B2: Campaign Optimization --------------------------\n")
     
     print("-------------------------- Running B3: Measuring Campaign ROI --------------------------\n")
-    B3_main()
+    #B3_main()
     
     print("-------------------------- Running B4: Cost Effectiveness of Campaigns --------------------------\n")
-    B4_main()
+    #B4_main()
     
     print("-------------------------- Running B5: Customer Retention Strategies --------------------------\n")
     B5_main()
