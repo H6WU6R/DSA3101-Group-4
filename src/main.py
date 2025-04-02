1919343c88ec88b316c145e67443fc3ef436b6db
l-zy123 <113766676+l-zy123@users.noreply.github.com>
2025-04-02 20:36:06 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-print("importing modules...")
+print("-------------------------- importing modules... --------------------------\n")
 from A1_Customer_Segmentation.A1_main import main as A1_main
 from A2_Customer_Engagement.A2_main import main as A2_main
 from A3_Behavioral_Patterns_Analysis.A3_main import main as A3_main
@@ -12,20 +12,20 @@ from B5_Customer_Retention_Strategies.B5_main import main as B5_main
 
 if __name__ == "__main__":
 
-    # print("-------------------------- Running A1: Customer Segmentation --------------------------\n")
-    # A1_main()
+    print("-------------------------- Running A1: Customer Segmentation --------------------------\n")
+    A1_main()
     
-    # print("-------------------------- Running A2: Customer Engagement --------------------------\n")
-    # A2_main()
+    print("-------------------------- Running A2: Customer Engagement --------------------------\n")
+    A2_main()
     
-    # print("-------------------------- Running A3: Behavioral Patterns --------------------------\n")
-    # A3_main()
+    print("-------------------------- Running A3: Behavioral Patterns --------------------------\n")
+    A3_main()
     
-    # print("-------------------------- Running A4: Campaign Impact Analysis --------------------------\n")
-    # A4_main()
+    print("-------------------------- Running A4: Campaign Impact Analysis --------------------------\n")
+    A4_main()
     
-    # print("-------------------------- Running A5: Segmentation Updates --------------------------\n")
-    # A5_app()
+    print("-------------------------- Running A5: Segmentation Updates --------------------------\n")
+    A5_app()
 
     print("-------------------------- Running B1: Predicting Customer Preferences --------------------------\n")
     B1_main()
